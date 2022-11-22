64ff87fab356f3d8d4f34f4f8b522a59f4cea0d1
Martin Vaccarezza <martin.vaccarezza@live.com>
2022-11-21 22:10:18 -0300

    Clase 3v01

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return(
+<nav className= 'navegacion'>
+    <a href="/#" >Inicio</a>
+    <a href="/#" >Productos</a>
+    <a href="/#" >Sobre Nosotros</a>
+    <a href="/#" >Contacto</a>
+    <button className='botonIngreso'>Ingreso</button>
+</nav>
+    )
+}
+
+export default Navbar
